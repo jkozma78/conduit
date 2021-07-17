4e80ddb0976d54288b34e4c9f57531c1185ae8f5
Kozma János <jkozma@freestart.hu>
2021-07-17 15:59:53 +0200

    first pytest

--- python_tests/test_conduit_sign_up.py
+++ python_tests/test_conduit_sign_up.py
@@ -1,13 +1,16 @@
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='session')
 def browser():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+    driver = webdriver.Chrome(options=options)
     driver.get("http://localhost:1667/")
     return driver
 
