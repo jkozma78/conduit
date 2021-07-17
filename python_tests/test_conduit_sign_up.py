0092a2bca58ca52abb059f7713accdc3c8143eac
Kozma János <jkozma@freestart.hu>
2021-07-17 15:51:18 +0200

    first pytest

---
+++ python_tests/test_conduit_sign_up.py
@@ -0,0 +1,36 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+@pytest.fixture(scope='session')
+def browser():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:1667/")
+    return driver
+
+
+def test_registration(browser):
+    
+    browser.find_element_by_xpath('//a[@href="#/register"]').click()
+    browser.find_element_by_xpath('//input[@placeholder="Username"]').send_keys('Jani')
+    browser.find_element_by_xpath('//input[@placeholder="Email"]').send_keys('jani@jani.hu')
+    browser.find_element_by_xpath('//input[@placeholder="Password"]').send_keys('Jani.1234')
+    browser.find_element_by_tag_name("button").click()
+
+    splash = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="swal-text"]')))
+    assert browser.find_element_by_xpath('//div[@class="swal-text"]').text == "Your registration was successful!"
+    assert splash.text == "Email already taken."
+    browser.find_element_by_xpath('//div[@class="swal-text"]').click()
+
+
+
+
+def test_login(browser):
+    browser.get('http://localhost:1667/')
+    browser.find_element_by_xpath('//a[@href="#/login"]').click()
+    browser.find_element_by_xpath('//input[@placeholder="Email"]').send_keys('jani@jani.hu')
+    browser.find_element_by_xpath('//input[@placeholder="Password"]').send_keys('Jani.12346')
+    browser.find_element_by_tag_name("button").click()
