69aee88dfe2076dbe08ebb74cbeea72ab523ea11
Kozma János <jkozma@freestart.hu>
2021-07-17 16:21:23 +0200

    first pytest

---
+++ python_tests/test_easy.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def test_one():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+    driver.get('http://localhost:1667')
+    assert driver.title == 'Conduit'
