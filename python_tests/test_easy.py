20bd5faa135b1c1fb3cd524db3b23f34a27a2f52
Kozma János <jkozma@freestart.hu>
2021-07-18 11:53:07 +0200

    'test'

--- python_tests/test_easy.py
+++ python_tests/test_easy.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 def test_one():
@@ -10,5 +12,7 @@ def test_one():
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     driver.get('http://localhost:1667/#/')
-    driver.find_element_by_xpath('//a[@href="#/login"]').click()
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//a[@href="#/login"]')))
+    # driver.find_element_by_xpath('//a[@href="#/login"]').click()
+    element.click()
     assert driver.find_element_by_tag_name('h1').text == 'Sign in'
