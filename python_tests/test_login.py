b555e20ef3573c937e7915efe20dd5288d5bf260
Kozma János <jkozma@freestart.hu>
2021-07-18 07:07:58 +0200

    test

--- python_tests/test_login.py
+++ python_tests/test_login.py
@@ -9,8 +9,8 @@ def browser():
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')  # Last I checked this was necessary.
     driver = webdriver.Chrome(options=options)
-    
-    driver.implicitly_wait(10)
+
+    driver.implicitly_wait(30)
     driver.get('http://localhost:1667')
     return driver
 
