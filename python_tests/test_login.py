8e08155f6cdad7691d3ce9fc5663f0cba613510b
Kozma János <jkozma@freestart.hu>
2021-07-18 07:04:51 +0200

    login test

--- python_tests/test_login.py
+++ python_tests/test_login.py
@@ -5,11 +5,11 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='session')
 def browser():
-    # options = Options()
-    # options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')  # Last I checked this was necessary.
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+    driver = webdriver.Chrome(options=options)
+    
     driver.implicitly_wait(10)
     driver.get('http://localhost:1667')
     return driver
