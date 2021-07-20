58ed94d96d59ba845aa40d0ff099abe43734e1ea
Kozma János <jkozma@freestart.hu>
2021-07-20 13:53:13 +0200

    pytest

--- python_tests/test_easy.py
+++ python_tests/test_easy.py
@@ -1,19 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+import pytest
 
-
-def test_one():
+@pytest.fixture(scope="session")
+def browser():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')  # Last I checked this was necessary.
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     driver.get('http://localhost:1667/#/')
-    driver.fullscreen_window()
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//a[@href="#/login"]')))
-    # driver.find_element_by_xpath('//a[@href="#/login"]').click()
-    element.click()
-    assert driver.find_element_by_tag_name('h1').text == 'Sign in'
+    return driver
+
+def test_one(browser):
+    browser.find_element_by_xpath('//a[@href="#/login"]').click()
+
+    assert browser.find_element_by_tag_name('h1').text == 'Sign in'
