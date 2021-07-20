d98ea7db3a3d248de437549c66bf2cdc0382d2d3
Kozma János <jkozma@freestart.hu>
2021-07-20 18:53:05 +0200

    test

--- python_tests/test_conduit.py
+++ python_tests/test_conduit.py
@@ -1,6 +1,11 @@
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
+
 
 @pytest.fixture(scope="session")
 def browser():
@@ -9,10 +14,27 @@ def browser():
     options.add_argument('--disable-gpu')  # Last I checked this was necessary.
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
-    driver.get('http://localhost:1667/#/')
+
     return driver
 
-def test_one(browser):
-    browser.find_element_by_xpath('//a[@href="#/login"]').click()
 
-    assert browser.find_element_by_tag_name('h1').text == 'Sign in'
+def test_register(browser):
+    browser.get('http://localhost:1667')
+    browser.find_element_by_xpath('//a[@href="#/register"]').click()
+    browser.find_element_by_xpath('//input[@placeholder="Username"]').send_keys("Jani")
+    browser.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("jani@jani.com")
+    browser.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Jani1234")
+    browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]').click()
+    # assert browser.find_element_by_xpath('//a[@href="#/@Jani/"]').text == "Jani"
+    a = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#/@Jani/']")))
+    assert a.text == "Jani"
+
+def test_login(browser):
+    browser.get('http://localhost:1667')
+    browser.find_element_by_xpath('//a[@href="#/login"]').click()
+    # browser.find_element_by_xpath('//input[@placeholder="Username"]').send_keys("Jani")
+    browser.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("jani@jani.com")
+    browser.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Jani1234")
+    browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]').click()
+    d = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#/@Jani/']")))
+    assert d.text == "Jani"
