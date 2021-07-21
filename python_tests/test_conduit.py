a56b7d233b3d92bcfa0d55cf0833b845ab02be73
Kozma János <jkozma@freestart.hu>
2021-07-21 16:53:30 +0200

    test

--- python_tests/test_conduit.py
+++ python_tests/test_conduit.py
@@ -1,13 +1,12 @@
-
+import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import pytest
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def browser():
     options = Options()
     options.add_argument('--headless')
@@ -17,25 +16,28 @@ def browser():
     return driver
 
 
+elemek = ('//a[@href="#/register"]', '//input[@placeholder="Username"]')
+
+
 def test_register(browser):
     browser.get('http://localhost:1667/#/')
-    browser.find_element_by_xpath('//a[@href="#/register"]').click()
-    browser.find_element_by_xpath('//input[@placeholder="Username"]').send_keys("Jani")
+    # browser.find_element_by_xpath('//a[@href="#/register"]').click()
+    browser.find_element_by_xpath(elemek[0]).click()
+    browser.find_element_by_xpath(elemek[1]).send_keys("Jani")
     browser.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("jani@jani.com")
     browser.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Jani1234")
     browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]').click()
-    # assert browser.find_element_by_xpath('//a[@href="#/@Jani/"]').text == "Jani"
-    a = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='swal-button-container']")))
-    a.click()
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//div[@class='swal-button-container']"))).click()
+
     browser.find_element_by_xpath('//a[@active-class="active"]').click()
 
+
 def test_login(browser):
-    # browser.get('http://localhost:1667/#/')
     browser.find_element_by_xpath('//a[@href="#/login"]').click()
-    # browser.find_element_by_xpath('//input[@placeholder="Username"]').send_keys("Jani")
     browser.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("jani@jani.com")
     browser.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Jani1234")
     browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]').click()
-    d = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//a[@href='#/@Jani/']")))
-    assert d.text == "Jani"
-
+    assert WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//a[@href='#/@Jani/']"))).text == "Jani"
+    browser.find_element_by_xpath('//a[@active-class="active"]').click()
