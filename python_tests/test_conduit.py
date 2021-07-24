aa7f55256960eeabc8e26f86f99532a10b8d3ad0
Kozma János <jkozma@freestart.hu>
2021-07-24 18:01:37 +0200

    test

--- python_tests/test_conduit.py
+++ python_tests/test_conduit.py
@@ -18,16 +18,18 @@ def browser():
 
 
 def find_locators(browser, xpath):
-    """a teszt során felhasznált xpath lokátorok egy locators dictionary-be rendezve"""
+    """xpath locators in locators dictionary"""
     locators = {"register": '//a[@href="#/register"]',
-                "Username": '//input[@placeholder="Username"]',
-                "Email": '//input[@placeholder="Email"]',
-                "Password": '//input[@placeholder="Password"]',
-                "Okbutton": '//button[@class="btn btn-lg btn-primary pull-xs-right"]',
-                "Modalbutton": "//div[@class='swal-button-container']",
+                "username": '//input[@placeholder="Username"]',
+                "email": '//input[@placeholder="Email"]',
+                "password": '//input[@placeholder="Password"]',
+                "okbutton": '//button[@class="btn btn-lg btn-primary pull-xs-right"]',
+                "modalbutton": "//div[@class='swal-button-container']",
                 "logout": '//a[@active-class="active"]',
                 "login": '//a[@href="#/login"]',
-                "accept": '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]'}
+                "accept": '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]',
+                "pages": '//a[@class="page-link"]'}
+
     return browser.find_element_by_xpath(locators[xpath])
 
 
@@ -37,15 +39,15 @@ def wait_for_element(browser, xpath):
         EC.visibility_of_element_located((By.XPATH, xpath)))
 
 
-# @pytest.mark.skip(reason="no way of currently testing this")
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_register(browser):
     """a user registration"""
     browser.get('http://localhost:1667/')
     find_locators(browser, "register").click()
-    find_locators(browser, "Username").send_keys("Jani")
-    find_locators(browser, "Email").send_keys("jani@jani.com")
-    find_locators(browser, "Password").send_keys("Jani1234")
-    find_locators(browser, "Okbutton").click()
+    find_locators(browser, "username").send_keys("Jani")
+    find_locators(browser, "email").send_keys("jani@jani.com")
+    find_locators(browser, "password").send_keys("Jani1234")
+    find_locators(browser, "okbutton").click()
     wait_for_element(browser, "//div[@class='swal-button-container']").click()
     find_locators(browser, "logout").click()
 
@@ -54,9 +56,9 @@ def test_login(browser):
     """login with a registered user"""
     browser.get('http://localhost:1667/')
     find_locators(browser, "login").click()
-    find_locators(browser, "Email").send_keys("jani@jani.com")
-    find_locators(browser, "Password").send_keys("Jani1234")
-    find_locators(browser, "Okbutton").click()
+    find_locators(browser, "email").send_keys("jani@jani.com")
+    find_locators(browser, "password").send_keys("Jani1234")
+    find_locators(browser, "okbutton").click()
     assert wait_for_element(browser, "//a[@href='#/@Jani/']").text == "Jani"
     find_locators(browser, "logout").click()
 
@@ -65,3 +67,11 @@ def test_accept_cookies(browser):
     """accept cookies"""
     browser.get('http://localhost:1667/#/')
     find_locators(browser, "accept").click()
+
+
+def test_pagination(browser):
+    """select last page and assert if it is 'page-item active' """
+    test_login(browser)
+    pages = browser.find_elements_by_xpath('//a[@class="page-link"]')
+    pages[-1].click()
+    assert int(browser.find_element_by_xpath('//li[@class="page-item active"]/a').get_property("text")) == len(pages)
